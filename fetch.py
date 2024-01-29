a94f1000b902835d632b889097c0a0cca599c656
CodeFace <icodeface@users.noreply.github.com>
2024-01-29 12:23:44 +0800

    add bera-artio tokens

--- fetch.py
+++ fetch.py
@@ -9,7 +9,8 @@ CHAIN_ID_MAP = {
     "169": "manta-pacific",
     "324": "zksync-era",
     "42161": "arbitrum",
-    "42766": "zkfair"
+    "42766": "zkfair",
+    "80085": "bera-artio",
 }
 
 def izumi():
@@ -53,5 +54,36 @@ def izumi():
             json.dump(new_tokens, writer, indent=2)
 
 
+def bera_artio():
+    data = requests.get("https://raw.githubusercontent.com/berachain/default-token-list/main/src/tokens/testnet/defaultTokenList.json").json()
+    result = {
+        "bera-artio": []
+    }
+    for item in data.get("tokens", []):
+        token = {
+            "address": item["address"],
+            "name": item["name"],
+            "symbol": item["symbol"],
+            "decimals": item["decimals"],
+            "logoURI": item["logoURI"],
+        }
+        result["bera-artio"].append(token)    
+    
+    for chain, tokens in result.items():
+        origin_tokens = []
+        origin_addrs = set()
+        with open(f"{chain}.json", "r") as reader:
+            origin_tokens = json.load(reader)
+        for t in origin_tokens:
+            origin_addrs.add(t["address"].lower())
+        new_tokens = origin_tokens
+        for t in tokens:
+            if t["address"].lower() in origin_addrs:
+                continue
+            new_tokens.append(t)
+        with open(f"{chain}.json", "w") as writer:
+            json.dump(new_tokens, writer, indent=2)
+
+
 if __name__ == "__main__":
-    izumi()
+    bera_artio()
